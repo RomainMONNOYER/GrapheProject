97fe380035c89d5448098c11fb7526ba55f8eaa3
BlueTorche <dubrulle_kevin@hotmail.be>
2021-12-18 17:24:08 +0100

    modif main

--- src/main.py
+++ src/main.py
@@ -2,16 +2,42 @@ from functions.lecture import *
 from functions.printSol import *
 from src.functions.Base.BoxFiting import *
 from functions.Splitter.smartIncreasing import *
-from functions.Metaheuristics.localSearch import *
+
+import functions.Metaheuristics.localSearch as localSearch
+import functions.Metaheuristics.recuit as recuit
+import functions.Metaheuristics.tabou as tabou
+import time
 
 if __name__ == '__main__':
     N,B,E,quantities = readData("datas/data10.dat")
     sol,decomp = base(N,B,E,quantities)
 
     printsol(sol,decomp)
+    #print("COST", sum([sol[i][0] for i in range(len(sol))]))
 
-    sol,decomp = run(N,B,E,quantities,sol,decomp)
-    printsol(sol,decomp)
+    print("\n============================================\nlocalSearch\n=========================================\n")
+    begin = time.time()
+    sol1,decomp1 = localSearch.run(N,B,E,quantities,sol,decomp)
+    printsol(sol1,decomp1)
+    #print("COST", sum([sol1[i][0] for i in range(len(sol1))]))
+    print("\ntime:", end = ' ')
+    print(time.time()-begin)
+
+    print("\n============================================\nrecuit\n=========================================\n")
+    begin = time.time()
+    sol2,decomp2 = recuit.run(N,B,E,quantities,sol,decomp)
+    printsol(sol2,decomp2)
+    #print("COST", sum([sol2[i][0] for i in range(len(sol2))]))
+    print("\ntime:", end = ' ')
+    print(time.time()-begin)
+
+    print("\n============================================\ntabou\n=========================================\n")
+    begin = time.time()
+    sol3,decomp3 = tabou.run(N,B,E,quantities,sol,decomp)
+    printsol(sol3,decomp3)
+    #print("COST", sum([sol3[i][0] for i in range(len(sol3))]))
+    print("\ntime:", end = ' ')
+    print(time.time()-begin)
 
     #To comment
     print(sum(quantities) == sum([sum([x for x in j]) for j in sol]))
